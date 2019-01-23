9962694d02894e5350859bbd31b8c73594726726
geoffroyvilain <vilaingeoffroy02@gmail.com>
2019-01-23 15:25:35 +0100

    exercice fini

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+function getSee() {
+  var input_value = document.getElementById('lastname').value;
+  alert("Vous venez de saisir : "+input_value);
+}
+// INPUT VALUE PERMET DE RECUPERER CE QUI EST SAISI DANS LE CHAMP
