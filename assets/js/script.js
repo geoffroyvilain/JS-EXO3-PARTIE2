7b3e9cbe27d1b16617fc20c179b7f0554eaa775e
geoffroyvilain <vilaingeoffroy02@gmail.com>
2019-01-28 14:05:01 +0100

    exercice corrigé

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
-function getSee() {
+function onKeyUp() {
+//  RECUPERER LA VALEUR DE L'INPUT GRACE A .VALUE  --  ET CREER LA VARIABLE AVEC VAR
   var input_value = document.getElementById('lastname').value;
   alert("Vous venez de saisir : "+input_value);
 }
-// INPUT VALUE PERMET DE RECUPERER CE QUI EST SAISI DANS LE CHAMP
+//  INPUT VALUE PERMET DE RECUPERER CE QUI EST SAISI DANS LE CHAMP
